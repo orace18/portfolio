171ea24ca0bd9efd7640f77776fae081cb0905fb
Orace Juscard <oracejuscard@gmail.com>
2026-07-07 20:45:40 +0100

    fix: ok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 // Configuration de l'environnement de PRODUCTION.
-// Remplacez apiUrl par l'URL publique de votre API NestJS déployée (Render, Railway, Fly.io, VM...).
 export const environment = {
   production: true,
-  apiUrl: 'https://VOTRE-API-EN-PRODUCTION.example.com',
+  apiUrl: 'https://portfolio-h45y.onrender.com',
 };
